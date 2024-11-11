d568e720b720e21400f7b943c3abccde80cfeb3f
Angerome22 <angeline.jerome12@gmail.com>
2024-11-11 19:07:39 +0100

    authentification user implémentée et fonctionnelle

---
+++ models/Book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema ({
+    userId: { type: String, required: true }, 
+    title: { type: String, required: true }, 
+    author: { type: String, required: true }, 
+    imageUrl: { type: String, required: true }, 
+    year: { type: Number, required: true }, 
+    genre: { type: String, required: true }, 
+    ratings: [ // Tableau des évaluations
+      {
+        userId: { type: String, required: true }, // ID de l'utilisateur qui a noté le livre
+        grade: { type: Number, required: true, min: 0, max: 5 } // Note attribuée, entre 0 et 5
+      }
+    ],
+    averageRating: { type: Number, default: 0 } // Note moyenne, calculée à partir des évaluations
+  });
+
+module.exports = mongoose.model('Book', bookSchema);
